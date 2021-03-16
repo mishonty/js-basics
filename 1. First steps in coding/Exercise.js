81cf4f064c2090341ab42c743b9ee4e0aa3d2373
mishonty <80749024+mishonty@users.noreply.github.com>
2021-03-16 12:24:03 +0200

    Update Exercise.js

--- 1. First steps in coding/Exercise.js
+++ 1. First steps in coding/Exercise.js
@@ -5,3 +5,113 @@ function usdToBgn(input) {
     console.log(bgn);
 }
 
+
+// 2. Radians to degrees
+function convert(input) {
+    let rad = Number(input);
+    let degree = rad * 180 / Math.PI;
+    console.log(degree.toFixed(0));
+}
+
+
+// 3. Deposit calculator
+function solve(input) {
+
+    let depositedSum = Number(input[0]);
+    let period = Number(input[1]);
+    let percent = Number(input[2]);
+
+    let result = depositedSum + period * ((depositedSum * percent / 100) / 12);
+
+    console.log(result);
+}
+
+
+// 4. Vacation book list
+function solve(input) {
+
+    let nbPages = Number(input[0]);
+    let pagesPerHour = Number(input[1]);
+    let days = Number(input[2]);
+
+    let hours = nbPages / pagesPerHour;
+
+    console.log(hours / days);
+}
+
+
+// 5. Birthday party
+function birthday(rent) {
+    rent = Number(rent);
+
+    let cake = rent * 0.2;
+    let drinks = cake - (cake * 0.45);
+    let animator = rent * 1/3;
+
+    let totalPrice = rent + cake + drinks + animator;
+    console.log(totalPrice)
+}
+
+
+// *6. Charity campaign
+function charity(input) {
+    days = Number(input[0]);
+    nberChefs = Number(input[1]);
+    cakesNumber = Number(input[2]);
+    gofrettesNumber = Number(input[3]);
+    pancakesNumber = Number(input[4]);
+
+    let cake = 45 * cakesNumber;
+    let gofrette = 5.8 * gofrettesNumber;
+    let pancake = 3.2 * pancakesNumber;
+
+    let finalSum = cake + gofrette + pancake;
+    finalSum = Number(finalSum);
+
+    let total = nberChefs * finalSum * days;
+    let expenses = total * 1/8;
+    let netTotal = total - expenses;
+
+    console.log(netTotal);
+}
+
+
+// *7. Fruit market
+function market(input) {
+    strPrice = Number(input[0]);
+    banana = Number(input[1]);
+    orange = Number(input[2]);
+    rasp = Number(input[3]);
+    str = Number(input[4]);
+
+    let raspberriesPrice = strPrice / 2;
+    let orangesPrice = raspberriesPrice * 0.6;
+    let bananaPrice = raspberriesPrice * 0.2;
+
+    strPrice = strPrice * str;
+    bananaPrice = bananaPrice * banana;
+    raspberriesPrice = raspberriesPrice * rasp;
+    orangesPrice = orangesPrice * orange;
+
+    let total = strPrice + bananaPrice + orangesPrice + raspberriesPrice;
+    
+    console.log(total);
+}
+
+
+// *8. Fish tank
+function aquarium(input){
+
+    length = Number(input[0]);
+    width = Number(input[1]);
+    height = Number(input[2]);
+    percent = Number(input[3]);
+    
+    let volume = length * width * height;
+    let l = volume * 0.001;
+    let percentNum = percent * 0.01;
+
+    console.log(l * (1 - percentNum));
+}
+
+
