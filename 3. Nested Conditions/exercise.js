e8fe373b84b7f742073881482a2699c25fbe6f95
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-03-21 11:33:15 +0200

    Update exercise.js

--- 3. Nested Conditions/exercise.js
+++ 3. Nested Conditions/exercise.js
@@ -48,3 +48,42 @@ function solve(input) {
 }
 
 
+// 4. Fishing boat
+function solve(input) {
+    budget = Number(input[0]);
+    season = input[1];
+    persons = input[2];
+
+    let rentPrice = 0;
+
+    switch (season) {
+        case "Spring":
+            rentPrice = 3000;
+            break;
+        case "Summer":
+        case "Autumn":
+            rentPrice = 4200;
+            break;
+        case "Winter":
+            rentPrice = 2600;
+            break;
+    }
+
+    if (persons <= 6) {
+        rentPrice *= 0.9;
+    } else if (persons <= 11) {
+        rentPrice *= 0.85;
+    } else {
+        rentPrice *= 0.75;
+    }
+
+    if (persons % 2 === 0 && season !== "Autumn") {
+        rentPrice *= 0.95;
+    }
+
+    if (rentPrice > budget) {
+        console.log(`Not enough money! You need ${(rentPrice - budget).toFixed(2)} leva.`);
+    } else {
+        console.log(`Yes! You have ${(budget - rentPrice).toFixed(2)} leva left.`);
+    }
+}
