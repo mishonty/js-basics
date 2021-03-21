677eea81538c55c3cf6e4c580cef449d70c58f6b
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-03-21 16:30:10 +0200

    Update exercise.js

--- 3. Nested Conditions/exercise.js
+++ 3. Nested Conditions/exercise.js
@@ -1,5 +1,76 @@
+// 1. Cinema
+function solve(input) {
+    type = input[0];
+    r = Number(input[1]);
+    c = Number(input[2]);
+
+    let income = 0;
+
+    switch (type) {
+        case "Premiere":
+            income = r * c * 12;
+            break;
+        case "Normal":
+            income = r * c * 7.5;
+            break;
+        case "Discount":
+            income = r * c * 5;
+            break;
+    }
+
+    console.log(`${income.toFixed(2)} leva`);
+
+}
+
+
+// 2. Summer outfit
+function solve(input) {
+    degrees = Number(input[0]);
+    partOfTheDay = input[1];
+
+    let outfit = 0;
+    let shoes = 0;
+
+    if (partOfTheDay === "Morning") {
+        if (degrees >= 10 && degrees <= 18) {
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+        } else if (degrees > 18 && degrees <= 24) {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        }
+    } else if (partOfTheDay === "Afternoon") {
+        if (degrees >= 10 && degrees <= 18) {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else if (degrees > 18 && degrees <= 24) {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        } else {
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+        }
+    } else {
+        if (degrees >= 10 && degrees <= 18) {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else if (degrees > 18 && degrees <= 24) {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    }
+
+    console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+}
+
+
 // 3. New house
-// 3 задача
 function solve(input) {
     let flowerType = input[0];
     let flowerCounter = Number(input[1]);
@@ -87,3 +158,130 @@ function solve(input) {
         console.log(`Yes! You have ${(budget - rentPrice).toFixed(2)} leva left.`);
     }
 }
+
+
+// *5. Journey
+function solve(input) {
+    budget = Number(input[0]);
+    season = input[1];
+
+    let totalSum = 0;
+    let holidayType = 0;
+    let destination = 0;
+
+
+    if (budget <= 100) {
+        destination = "Bulgaria";
+        if (season === "summer") {
+            holidayType = "Camp";
+            totalSum = budget * 0.3;
+        } else {
+            holidayType = "Hotel";
+            totalSum = budget * 0.7;
+        }
+    } else if (budget <= 1000) {
+        destination = "Balkans";
+        if (season === "summer") {
+            holidayType = "Camp";
+            totalSum = budget * 0.4;
+        } else {
+            holidayType = "Hotel";
+            totalSum = budget * 0.8;
+        }
+    } else {
+        destination = "Europe";
+        holidayType = "Hotel";
+        totalSum = budget * 0.9;
+    }
+
+    console.log(`Somewhere in ${destination}`);
+    console.log(`${holidayType} - ${totalSum.toFixed(2)}`);
+    
+}
+
+// *6. Operations between numbers
+function solve(input) {
+    n1 = Number(input[0]);
+    n2 = Number(input[1]);
+    operator = input[2];
+
+    let result = 0;
+
+    if (n2 === 0) {
+        console.log(`Cannot divide ${n1} by zero`);
+    } else if (operator === "+") {
+        result = n1 + n2;
+        if (result % 2 === 0) {
+            console.log(`${n1} ${operator} ${n2} = ${result} - even`);
+        } else {
+            console.log(`${n1} ${operator} ${n2} = ${result} - odd`);
+        }
+    } else if (operator === "-") {
+        result = n1 - n2;
+        if (result % 2 === 0) {
+            console.log(`${n1} ${operator} ${n2} = ${result} - even`);
+        } else {
+            console.log(`${n1} ${operator} ${n2} = ${result} - odd`);
+        }
+    } else if (operator === "*") {
+        result = n1 * n2;
+        if (result % 2 === 0) {
+            console.log(`${n1} ${operator} ${n2} = ${result} - even`);
+        } else {
+            console.log(`${n1} ${operator} ${n2} = ${result} - odd`);
+        }
+    } else if (operator === "/") {
+        result = n1 / n2;
+        console.log(`${n1} / ${n2} = ${result.toFixed(2)}`);
+    } else if (operator === "%") {
+        result = n1 % n2;
+        console.log(`${n1} % ${n2} = ${result}`)
+    }
+}
+
+
+// *7. Hotel room
+function solve(input) {
+    month = input[0];
+    n = Number(input[1]);
+
+    let studio = 0;
+    let apartment = 0;
+
+    switch (month) {
+        case "May":
+        case "October":
+            studio = 50;
+            apartment = 65;
+            if (n > 14) {
+                studio *= 0.7;
+            } else if (n > 7) {
+                studio *= 0.95;
+            }
+            break;
+        case "June":
+        case "September":
+            studio = 75.2;
+            apartment = 68.7;
+            if (n > 14) {
+                studio *= 0.8;
+            }
+            break;
+        case "July":
+        case "August":
+            studio = 76;
+            apartment = 77;
+            break;
+    }
+
+    if (n > 14) {
+        apartment *= 0.9;
+    }
+
+    console.log(`Apartment: ${(apartment * n).toFixed(2)} lv.`);
+    console.log(`Studio: ${(studio * n).toFixed(2)} lv.`);
+
+}
+
+
+// *8. 
