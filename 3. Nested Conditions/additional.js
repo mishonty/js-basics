c93911e7b1dd9c609068939331a267980bff4c09
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-03-27 09:48:09 +0200

    Update additional.js

--- 3. Nested Conditions/additional.js
+++ 3. Nested Conditions/additional.js
@@ -302,3 +302,19 @@ function solve(input) {
     }
     
 }
+
+
+// *9. Numbers from 1 to 10
+function solve(input) {
+
+    let i = 1;
+
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+
+}
+
+
+// *10. Multiply by 2
