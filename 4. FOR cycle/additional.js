929815508e5d8d816c0810e265c250a87eb45d58
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-03-28 22:04:28 +0300

    Update additional.js

--- 4. FOR cycle/additional.js
+++ 4. FOR cycle/additional.js
@@ -1,1 +1,56 @@
 // 1. Back to the past
+function solve(input) {
+    money = Number(input[0]);
+    year = Number(input[1]);
+
+    let age = 18;
+
+    for (let i = 1800; i <= year; i++) {
+        if (i % 2 === 0) {
+            money -= 12000;
+        } else {
+            money -= 12000 + age * 50;
+        }
+        age++;
+    }
+
+    if (money >= 0) {
+        console.log(`Yes! He will live a carefree life and will have ${money.toFixed(2)} dollars left.`);
+    } else {
+        console.log(`He will need ${(Math.abs(money)).toFixed(2)} dollars to survive.`);
+    }
+
+}
+
+
+// 2. Hospital
+function solve(input) {
+    let period = Number(input[0]);
+
+    let treated = 0;
+    let untreated = 0;
+    let doctors = 7;
+
+    for (let i = 1; i <= period; i++) {
+
+        let patients = Number(input[i]);
+
+        if (i % 3 === 0 && untreated > treated) {
+            doctors++;
+        }
+
+        if (patients <= doctors) {
+            treated += patients;
+        } else {
+            treated += doctors;
+            untreated += patients - doctors;
+        }
+    }
+
+    console.log(`Treated patients: ${treated}.`);
+    console.log(`Untreated patients: ${untreated}.`);
+
+}
+
+
+// 3. Logistics
