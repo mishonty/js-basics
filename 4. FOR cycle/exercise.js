3e11912ad8958a99315fee5c9b81de41bec9012f
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-03-29 18:42:28 +0300

    Update exercise.js

--- 4. FOR cycle/exercise.js
+++ 4. FOR cycle/exercise.js
@@ -171,3 +171,31 @@ function solve(input) {
     }
 
 }
+
+
+// *. Salary (second code)
+function solve(input) {
+
+    let tabsNumber = Number(input[0]);
+    let salary = Number(input[1]);
+
+    for (let i = 2; i <= tabsNumber; i++) {
+
+        let tab = input[i];
+
+        if (tab === "Facebook") {
+            salary -= 150;
+        } else if (tab === "Instagram") {
+            salary -= 100;
+        } else if (tab === "Reddit") {
+            salary -= 50;
+        }
+    }
+
+    if (salary <= 0) {
+        console.log(`You have lost your salary.`);
+    } else {
+        console.log(salary.toFixed(0));
+    }
+
+}
